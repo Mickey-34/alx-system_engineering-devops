3de7f5e1a9be51cf5988cec86daba06684ddc6bf
Mickey-34 <128931001+Mickey-34@users.noreply.github.com>
2024-02-26 18:33:53 +0000

    Delete 0x0A-configuration_management directory
    delete

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,7 +0,0 @@
-# Creating A file in tmp folder named school
-file { '/tmp/school':
-    ensure  => 'present',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet', }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Kill a process
-
-exec { 'pkill killmenow':
-  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
-  returns => [0, 1],
-}
